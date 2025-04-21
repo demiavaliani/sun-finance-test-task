f916c40210ca9fb9eca0fdf89d7496b8a6b060da
Demi Avaliani <demikoavaliani@gmail.com>
2025-04-21 18:40:08 +0400

    chore: init - added necessary packages

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	modules: [
+		'@nuxt/eslint',
+		'nuxt-lucide-icons',
+		'@nuxtjs/tailwindcss',
+		'@vueuse/nuxt',
+		'@nuxtjs/google-fonts',
+	],
+	typescript: {
+		typeCheck: true,
+	},
+	googleFonts: {
+		families: {
+			'National Park': {
+				wght: [200, 400, 700],
+			},
+		},
+	},
+});
